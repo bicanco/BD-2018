1b87da3ac8028f5a8d737a52e0775db307189cc7
Marcos Wendell Souza de Oliveira Santos <wendell_mega@hotmail.com>
2018-06-21 17:56:01 -0300

    Search Screen

--- BD/src/UserInterface/SearchScreenController.java
+++ BD/src/UserInterface/SearchScreenController.java
@@ -1,15 +1,11 @@
 package UserInterface;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextField;
 import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -40,217 +36,40 @@ public class SearchScreenController implements Initializable {
     private JFXButton view;
     
     @FXML
-    private JFXTextField cnpjEmpresa;
+    private JFXComboBox<String> locacaoBusca;
     @FXML
-    private JFXTextField nomeEmpresa;
-    @FXML
-    private JFXTextField razaoEmpresa;
-    @FXML
-    private JFXTextField enderecoEmpresa;
-    @FXML
-    private JFXComboBox<String> tipoEmpresa;
-    @FXML
-    private JFXButton buscarEmpresa;
+    private JFXButton buscaLotes;
     
     @FXML
-    private JFXTextField empresaContatos;
+    private JFXComboBox<String> coquetelBusca;
     @FXML
-    private JFXTextField nomeContatos;
-    @FXML
-    private JFXTextField emailContatos;
-    @FXML
-    private JFXTextField telefoneContatos;
-    @FXML
-    private JFXButton buscarContatos;
+    private JFXButton buscaOrcamento;
     
     @FXML
-    private JFXTextField nomeCatFornecimento;
+    private JFXComboBox<String> funcionarioBusca;
     @FXML
-    private JFXTextField descricaoCatFornecimento;
+    private JFXComboBox<String> mesInicial;
     @FXML
-    private JFXButton buscarCatFornecimento;
+    private JFXComboBox<String> anoInicial;
+    @FXML
+    private JFXComboBox<String> mesFinal;
+    @FXML
+    private JFXComboBox<String> anoFinal;
+    @FXML
+    private JFXButton buscaFolhaPagamento;
     
     @FXML
-    private JFXTextField fornecedoraCatFornecedora;
+    private JFXComboBox<String> estadoBusca;
     @FXML
-    private JFXTextField categoriaCatFornecedora;
-    @FXML
-    private JFXTextField precoCatFornecedora;
-    @FXML
-    private JFXButton buscarCatFornecedora;
+    private JFXButton buscaMediaSalarial;
     
     @FXML
-    private JFXTextField contratanteFesta;
+    private JFXComboBox<String> categoriaBusca;
     @FXML
-    private JFXTextField dataFesta;
-    @FXML
-    private JFXTextField nomeFesta;
-    @FXML
-    private JFXTextField horarioFesta;
-    @FXML
-    private JFXTextField duracaoFesta;
-    @FXML
-    private JFXComboBox<String> tipoFesta;
-    @FXML
-    private JFXButton buscarFesta;
+    private JFXButton buscaFaixaPreco;
     
     @FXML
-    private JFXTextField festaFestFood;
-    @FXML
-    private JFXTextField precoFestFood;
-    @FXML
-    private JFXButton buscarFestFood;
-    
-    @FXML
-    private JFXTextField festfoodIngresso;
-    @FXML
-    private JFXTextField codigoIngresso;
-    @FXML
-    private JFXTextField compradorIngresso;
-    @FXML
-    private JFXButton buscarIngresso;
-    
-    @FXML
-    private JFXTextField festaCoquetel;
-    @FXML
-    private JFXTextField localCoquetel;
-    @FXML
-    private JFXTextField cidadeCoquetel;
-    @FXML
-    private JFXButton buscarCoquetel;
-    
-    @FXML
-    private JFXTextField nomeConvidado;
-    @FXML
-    private JFXTextField emailConvidado;
-    @FXML
-    private JFXTextField telefoneConvidado;
-    @FXML
-    private JFXButton buscarConvidado;
-    
-    @FXML
-    private JFXTextField coquetelConvite;
-    @FXML
-    private JFXTextField convidadoConvite;
-    @FXML
-    private JFXButton buscarConvite;
-    
-    @FXML
-    private JFXTextField coquetelBrinde;
-    @FXML
-    private JFXTextField nomeBrinde;
-    @FXML
-    private JFXTextField descricaoBrinde;
-    @FXML
-    private JFXButton buscarBrinde;
-    
-    @FXML
-    private JFXTextField fornecedoraFornecimento;
-    @FXML
-    private JFXTextField coquetelFornecimento;
-    @FXML
-    private JFXTextField precoFornecimento;
-    @FXML
-    private JFXButton buscarFornecimento;
-    
-    @FXML
-    private JFXTextField fornecedoraProdutos;
-    @FXML
-    private JFXTextField coquetelProdutos;
-    @FXML
-    private JFXTextField nomeProdutos;
-    @FXML
-    private JFXTextField quantidadeProdutos;
-    @FXML
-    private JFXButton buscarProdutos;
-    
-    @FXML
-    private JFXTextField nomeLocal;
-    @FXML
-    private JFXTextField ruaLocal;
-    @FXML
-    private JFXTextField numeroLocal;
-    @FXML
-    private JFXTextField cidadeLocal;
-    @FXML
-    private JFXTextField estadoLocal;
-    @FXML
-    private JFXTextField capacidadeLocal;
-    @FXML
-    private JFXTextField diariaLocal;
-    @FXML
-    private JFXCheckBox aberturaLocal;
-    @FXML
-    private JFXButton buscarLocal;
-    
-    @FXML
-    private JFXTextField festfoodLocacao;
-    @FXML
-    private JFXTextField localLocacao;
-    @FXML
-    private JFXTextField cidadeLocacao;
-    @FXML
-    private JFXButton buscarLocacao;
-    
-    @FXML
-    private JFXTextField fornecedoraLote;
-    @FXML
-    private JFXTextField locacaoLote;
-    @FXML
-    private JFXTextField numeroLote;
-    @FXML
-    private JFXTextField precoLote;
-    @FXML
-    private JFXTextField larguraLote;
-    @FXML
-    private JFXTextField comprimentoLote;
-    @FXML
-    private JFXButton buscarLote;
-    
-    @FXML
-    private JFXTextField cpfFuncionario;
-    @FXML
-    private JFXTextField nomeFuncionario;
-    @FXML
-    private JFXTextField rgFuncionario;
-    @FXML
-    private JFXTextField ruaFuncionario;
-    @FXML
-    private JFXTextField numeroFuncionario;
-    @FXML
-    private JFXTextField cidadeFuncionario;
-    @FXML
-    private JFXTextField estadoFuncionario;
-    @FXML
-    private JFXTextField emailFuncionario;
-    @FXML
-    private JFXTextField telefoneFuncionario;
-    @FXML
-    private JFXTextField celularFuncionario;
-    @FXML
-    private JFXTextField valorFuncionario;
-    @FXML
-    private JFXTextField funcaoFuncionario;
-    @FXML
-    private JFXButton buscarFuncionario;
-    
-    @FXML
-    private JFXTextField coquetelContCoquetel;
-    @FXML
-    private JFXTextField funcionarioContCoquetel;
-    @FXML
-    private JFXTextField horastrabContCoquetel;
-    @FXML
-    private JFXButton buscarContCoquetel;
-    
-    @FXML
-    private JFXTextField festfoodContFestFood;
-    @FXML
-    private JFXTextField segurancaContFestFood;
-    @FXML
-    private JFXTextField horastrabContFestFood;
-    @FXML
-    private JFXButton buscarContFestFood;
+    private JFXButton buscaProxFestFoods;
     
     private static Stage myStage;
     
@@ -263,19 +82,6 @@ public class SearchScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         myStage = Main.getMyStage();
-        
-        ObservableList<String> tipoempresa = 
-        	    FXCollections.observableArrayList(
-        	        "Fornecedora",
-        	        "Contratante",
-        	        "Ambas");
-        tipoEmpresa.setItems(tipoempresa);
-        
-        ObservableList<String> tipofesta = 
-        	    FXCollections.observableArrayList(
-        	        "Fest Food",
-        	        "Coquetel de Lançamento");
-        tipoFesta.setItems(tipofesta);
     }    
     
     @FXML
@@ -339,233 +145,32 @@ public class SearchScreenController implements Initializable {
     }
    
     @FXML
-    void buscarEmpresa(ActionEvent event) throws IOException{
-    	/*
-    	Empresa e = new Empresa(cnpjEmpresa.getText(), nomeEmpresa.getText(), razaoEmpresa.getText(), enderecoEmpresa.getText(), tipoEmpresa.getValue());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		Empresa.buscar(e);
-    		ou
-    		e.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    
-    @FXML
-    void buscarContato(ActionEvent event) throws IOException{
-    	/*
-    	ContatoEmpresa c = new ContatoEmpresa(empresaContatos.getText(), nomeContatos.getText(), emailContatos.getText(), telefoneContatos.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
+    void buscarLotes(ActionEvent event) throws IOException{
+
     }
     
     @FXML
-    void buscarCategoriaFornecimento(ActionEvent event) throws IOException{
-    	/*
-    	CategoriaFornecimento c = new CategoriaFornecimento(nomeCatFornecimento.getText(), descricaoCatFornecimento.getText());
+    void buscarOrcamento(ActionEvent event) throws IOException{
     	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-    	*/
     }
     
     @FXML
-    void buscarCategoriaFornecedora(ActionEvent event) throws IOException{
-    	/*
-    	CategoriaFornecedora c = new CategoriaFornecedora(fornecedoraCatFornecedora.getText(), categoriaCatFornecedora.getText(), precoCatFornecedora.getText());
+    void buscarFolhaPagamento(ActionEvent event) throws IOException{
     	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
     }
     
     @FXML
-    void buscarFesta(ActionEvent event) throws IOException{
-    	/*
-    	Festa f = new Festa(contratanteFesta.getText(), dataFesta.getText(), nomeFesta.getText(), horarioFesta.getText(), duracaoFesta.getText(), tipoFesta.getValue());
+    void buscarMediaSalarial(ActionEvent event) throws IOException{
     	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		f.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
     }
     
     @FXML
-    void buscarFestFood(ActionEvent event) throws IOException{
-    	/*
-    	FestFood f = new Festa(festaFestFood.getText(), precoFestFood.getText());
+    void buscarFaixaPreco(ActionEvent event) throws IOException{
     	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		f.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
     }
     
     @FXML
-    void buscarIngresso(ActionEvent event) throws IOException{
-    	/*
-    	Ingresso i = new Ingresso(festfoodIngresso.getText(), codigoIngresso.getText(), compradorIngresso.getText());
+    void buscarProxFestFoods(ActionEvent event) throws IOException{
     	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		i.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarCoquetel(ActionEvent event) throws IOException{
-    	/*
-    	Coquetel c = new Coquetel(festaCoquetel.getText(), localCoquetel.getText(), cidadeCoquetel.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarConvidado(ActionEvent event) throws IOException{
-    	/*
-    	Convidado c = new Convidado(nomeConvidado.getText(), emailConvidado.getText(), telefoneConvidado.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarConvite(ActionEvent event) throws IOException{
-    	/*
-    	Convite c = new Convite(coquetelConvite.getText(), convidadoConvite.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		c.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarBrinde(ActionEvent event) throws IOException{
-    	/*
-    	Brinde b = new Brinde(coquetelBrinde.getText(), nomeBrinde.getText(), descricaoBrinde.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		b.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarFornecimentoCoquetel(ActionEvent event) throws IOException{
-    	/*
-    	FornecimentoCoquetel f = new FornecimentoCoquetel(fornecedoraFornecimento.getText(), coquetelFornecimento.getText(), precoFornecimento.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		f.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarProduto(ActionEvent event) throws IOException{
-    	/*
-    	Produtos p = new Produtos(fornecedoraProdutos.getText(), coquetelProdutos.getText(), nomeProdutos.getText(), quantidadeProdutos.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		p.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarLocal(ActionEvent event) throws IOException{
-    	/*
-    	Local l = new Local(nomeLocal.getText(), ruaLocal.getText(), numeroLocal.getText(), cidadeLocal.getText(), estadoLocal.getText(), capacidadeLocal.getText(), diariaLocal.getText(), aberturaLocal.getValue());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		l.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarLocacao(ActionEvent event) throws IOException{
-    	/*
-    	Locacao l = new Locacao(festfoodLocacao.getText(), localLocacao.getText(), cidadeLocacao.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		l.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarLote(ActionEvent event) throws IOException{
-    	/*
-    	Lote l = new Lote(fornecedoraLote.getText(), locacaoLote.getText(), numeroLote.getText(), precoLote.getText(), larguraLote.getText(), comprimentoLote.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		l.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarFuncionario(ActionEvent event) throws IOException{
-    	/*
-    	Funcionario f = new Funcionario(cpfFuncionario.getText(), nomeFuncionario.getText(), rgFuncionario.getText(), numeroFuncionario.getText(), ruaFuncionario.getText(), cidadeFuncionario.getText(), estadoFuncionario.getText(), emailFuncionario.getText(), telefoneFuncionario.getText(), celularFuncionario.getText(), valorFuncionario.getText(), funcaoFuncionario.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		f.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarContratoCoquetel(ActionEvent event) throws IOException{
-    	/*
-    	ContratoCoquetel cc = new ContratoCoquetel(coquetelContCoquetel.getText(), funcionarioContCoquetel.getText(), horastrabContCoquetel.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		cc.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
-    }
-    
-    @FXML
-    void buscarContratoFestFood(ActionEvent event) throws IOException{
-    	/*
-    	ContratoFestFood cf = new ContratoFestFood(festfoodContFestFood.getText(), segurancaContFestFood.getText(), horastrabContFestFood.getText());
-    	
-    	if(verificação dos atributos para saber se estão conforme as especificações)
-    		cf.buscar();
-    	else
-    		tratamento dos erros (telas);
-   		*/
     }
 }
